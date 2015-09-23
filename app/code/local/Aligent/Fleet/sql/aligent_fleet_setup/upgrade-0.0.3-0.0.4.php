06076168cdf7b736041073f0b064383878555a17
Jim O'Halloran <jim@aligent.com.au>
2015-09-23 15:57:49 +0930

    Add index across both directory and filename to improve performance.

---
+++ app/code/local/Aligent/Fleet/sql/aligent_fleet_setup/upgrade-0.0.3-0.0.4.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Apply Anchor's recommended index to the core_file_storage table.
+ * Ref: http://docs.anchorfleet.com/fleet-magento-1/troubleshooting/cms-images/
+ *
+ * @author Jim O'Halloran <jim@aligent.com.au>
+ */
+
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+if (!array_key_exists('IDX_CORE_FILE_STORAGE_DIRECTORY_FILENAME', $installer->getConnection()->getIndexList($this->getTable('core_file_storage')))) {
+
+    $installer->run("CREATE INDEX IDX_CORE_FILE_STORAGE_DIRECTORY_FILENAME ON {$this->getTable('core_file_storage')} (directory ASC, filename ASC);");
+
+}
+
+$installer->endSetup();
